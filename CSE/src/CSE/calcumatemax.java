190c6c0e8d77dfed8f5adb7e4bc2d1ec735bf8f7
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2024-11-03 18:30:08 +0100

    Update calcumatemax.java

--- CSE/src/CSE/calcumatemax.java
+++ CSE/src/CSE/calcumatemax.java
@@ -4,14 +4,14 @@ public class calcumatemax {
 	public static int calculateMax(int[] arr) {
 	    int max = Integer.MIN_VALUE;
 
-	    // Find the max value in the array
+	    
 	    for (int i = 0; i < arr.length; i++) {
 	        if (arr[i] > max) {
 	            max = arr[i];
 	        }
 	    }
 
-	    // If the max is zero, add one to each element and try again
+	    
 	    if (max == 0) {
 	        for (int i = 0; i < arr.length; i++) {
 	            arr[i]++;
